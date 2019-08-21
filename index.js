e7b777961ce294e7977fcbe64598bd3e32e98ec8
Aaron Sickler <sicklera517@gmail.com>
2019-08-21 18:01:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function sayHiToGrandma(string) {
   if(string.toUpperCase() === string){
   return "YES INDEED!"
 }
-  if(string === I"ilove you, Grandma."){
+  if(string === "I love you, Grandma."){
   return "I love you too"
 }
