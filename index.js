32cb70cf7841582aeb25389c2aa2b1fb283c54d8
Aaron Sickler <sicklera517@gmail.com>
2019-08-21 17:38:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function shout(string) {
   return string.toUpperCase()
 }
 
+function whisper(string) {
+  return string.toLowerCase()
+}
+
 function sayHiToGrandma(string) {
   if(string === "I love you, Grandma.")
   return "I love you, too."
